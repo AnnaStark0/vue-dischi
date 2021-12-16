68b6c8b661a60c9b96328d277e9fa7f27bd07bc5
anne <89winteriscoming@gmail.com>
2021-12-16 15:21:16 +0100

    Header

--- src/components/Macro/Header.vue
+++ src/components/Macro/Header.vue
@@ -1,15 +1,31 @@
 <template>
-  <header>
-      <h1>Classic Rock</h1>
+  <header class="py-3">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+
+          <img src="../../assets/img/logo.png" alt="" />
+
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-name: 'Header'
-}
+  name: "Header",
+};
 </script>
 
 <style lang="scss" scoped>
+header {
+  background-color: rgb(109, 109, 109);
+
+  img {
+      width: 50px;
+  }
+}
+
 
 </style>
