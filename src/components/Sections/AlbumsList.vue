a44db6b57fb06dca204f03cdbc8bc0ca69acf043
anne <89winteriscoming@gmail.com>
2021-12-16 16:32:10 +0100

    Axios

--- src/components/Sections/AlbumsList.vue
+++ src/components/Sections/AlbumsList.vue
@@ -26,12 +26,31 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 import AlbumCard from '../Commons/AlbumCard.vue';
 
 export default {
 name: 'AlbumsList',
 components: {
     AlbumCard
+},
+data () {
+return {
+    albums: null
+}
+},
+created () {
+
+axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+  .then ((response) => {
+    // handle success
+    this.albums = response.data.response;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
 }
 };
 </script>
